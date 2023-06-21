de019362f41c0ba17ce1ec5870d5073f40198ecb
Muhammad Urwah <urwahpatel@gmail.com>
2023-06-22 00:31:19 +0500

    Updated Description

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <h1 className='mx-auto tracking-tighter text-3xl md:text-6xl 
       font-extrabold text-center my-4 md:w-[630px]
       '>
-        Generate Explanations of any Term you Enter
+        Generate Explanations of any Term
       </h1>
       <Form resultRef={resultRef}/>
     </main> 
